c4414d14cc04b5aacee7c104aef58041ca67b4d1
intelligentchild <shiv.kurtartkar@gmail.com>
2020-10-17 21:48:02 +0530

    dev:mouse controll script

--- main.py
+++ main.py
@@ -1,44 +1,99 @@
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from datetime import datetime
+import time
 
-import mouseController as mc
+from  mouseController import *
 
 maxListLen = 20
+mc = None
+
+def log(txt):
+	now = datetime.now()
+	log = str(now)+' :: '+txt
+	logList.insert(END, log)
+	
+	while logList.size() >= maxListLen:
+		logList.delete(0)
+	logList.see(END)
+
+mc = Controller(callback =log)
 
 def toggleState():
 	print('test')
 	print(actionBtnTxt.get())
 	isRunning = (actionBtnTxt.get()=='Start')
 	if isRunning:
-		mc.loop()
+		mc.start()
+	else:
+		mc.stop()
 	btnTxt =  'Stop' if isRunning else 'Start'
 	actionBtnTxt.set(btnTxt)
-	now = datetime.now()
-	log = str(now)+' '+btnTxt
-	logList.insert(END, log)
-	length = logList.size()
-	if length >= maxListLen:
-		logList.delete(0)
-	logList.see(END)
+
+def reloadScript():
+	print('reloading script')
+
+def clearLogs():
+	print('clearing Logs')
+	logList.delete(0,END)
+
+def logCursor():
+	loggingState =  mc.toggleCursorLogging()
+	print('logging cursor ', loggingState)
+	txt = 'Log Cursor Cords' + (' (ON)' if loggingState else '')
+	logCursorLocBtnTxt.set(txt)
+
+def on_closing():
+	print('close event..')
+	mc.toggleCursorLogging(False)
+	mc.stop()
+	mc.stopThread()
+	root.quit()
 
 root = Tk()
 root.title('Automate')
 
-root.columnconfigure(1,weight=1)
-root.rowconfigure(1,weight=1)
+root.columnconfigure(2,weight=1)
+root.rowconfigure(2,weight=1)
 
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+row=0
 # Parent widget for buttons
 btnFrame = Frame(root)
-btnFrame.grid(row=0, column=0, sticky=W+E)
+btnFrame.grid(row=row, column=0, sticky=W+E)
 
 actionBtnTxt = StringVar()
 actionBtn = Button(btnFrame, textvariable=actionBtnTxt, command=toggleState)
 actionBtnTxt.set('Start')
 actionBtn.grid(row=0,column=0, padx=(10), pady=10)
 
+reloadScriptBtn= Button(btnFrame, text='Reload Script', command=reloadScript)
+reloadScriptBtn.grid(row=0, column=1, padx=(10), pady=10)
+
+clearLogsBtn = Button(btnFrame, text='Clear Logs',command=clearLogs)
+clearLogsBtn.grid(row=0, column=2, padx=(10), pady=10)
+
+logCursorLocBtnTxt = StringVar()
+logCursorLocBtn = Button(btnFrame, textvariable=logCursorLocBtnTxt,command=logCursor)
+logCursorLocBtnTxt.set('Log Cursor Cords')
+logCursorLocBtn.grid(row=0, column=3, padx=(10), pady=10)
+
+row=row+1
+# script frame
+scriptFrame = LabelFrame(root, text='Script', padx=5, pady=5)
+scriptFrame.grid(row=row, column=0, columnspan=3, padx=10, pady=10, sticky=E+W+N+S)
+
+scriptFrame.columnconfigure(0, weight=1)
+scriptFrame.rowconfigure(0,  weight=1)
+
+scriptTxt = ScrolledText(scriptFrame, wrap=WORD, height=7)
+scriptTxt.grid(row=0,column=0, sticky=E+W+N+S)
+
+row=row+1
 # Log frame
 logFrame = LabelFrame(root, text='Logs', padx=5, pady=5)
-logFrame.grid(row=1, column=0, columnspan=3, padx=10,pady=10, sticky=E+W+N+S)
+logFrame.grid(row=row, column=0, columnspan=3, padx=10,pady=10, sticky=E+W+N+S)
 
 logFrame.columnconfigure(0, weight=1)
 logFrame.rowconfigure(0,  weight=1)
--- mouseController.py
+++ mouseController.py
@@ -1,8 +1,115 @@
 import mouse
+import time
+import threading
 
-def click():
-	print("Click")
 
-def loop():
-	for i in range (10):
-		click()
+def dummy(str):
+	pass
+
+def click(x, y):
+	curr = mouse.get_position()
+	mouse.move(x, y, absolute=True, duration=0.0)
+	mouse.click('left')
+	mouse.move(curr[0], curr[1])
+
+class Controller():
+	def __init__(self, logCursor=False, start=False, callback=None):
+		self.logCursor =logCursor
+		self.isRunning = start
+		
+		self.thread = None
+		if callback is None:
+			self.collback = dummy
+		else:
+			self.callback=callback
+	
+	def __del__(self):
+		self.stopThread()
+
+	def toggleCursorLogging(self, state=None):
+		if state is not None:
+			self.logCursor = state
+		else:
+			self.logCursor = not self.logCursor
+		if self.logCursor:
+			self.startThread()
+		else:
+			self.stopThread()
+		return self.logCursor
+
+	def cursorLoggingStatus(self):
+		return self.logCursor
+
+	def start(self):
+		self.isRunning = True
+		# start the thread
+		self.startThread()
+
+	def stop(self):
+		self.isRunning = False
+		#stop the thread
+		self.stopThread()
+	
+	def status(self):
+		return self.isRunning
+	
+	def startThread(self):
+		print('starting thread')
+		if self.thread is None or not self.thread.is_alive():
+			self.thread = threading.Thread(target=self.loop, args=())
+			self.thread.start()
+		else :
+			print('Thread already running')
+	def stopThread(self):
+		if self.thread is None:
+			print('Thread not running')
+		else:
+			if self.isRunning or self.logCursor:
+				print('cant terminate thread')
+			else:
+				print('thread status:',self.thread.is_alive())
+				if self.thread.is_alive():
+					#stop thread
+					self.isRunning= False
+					self.logCursor= False
+					print('Waiting for thread to terminate')
+					if self.thread is not None and self.thread.is_alive():
+						self.thread.join()
+					print('Thread terminated')
+					self.thread =None
+				else:
+					print('Thread already terminated')
+		
+	def loop(self):
+		print('executing loop')
+		currentTime = time.time()
+		lastFrameTime = currentTime
+		FPS = 2
+		
+		while self.isRunning or self.logCursor:
+			currentTime = time.time()
+			self.update(currentTime)
+			sleepTime = 1./FPS - (currentTime - lastFrameTime)
+			lastFrameTime = currentTime
+			if sleepTime > 0:
+				time.sleep(sleepTime)
+			print('sleepTime:',sleepTime)			
+			
+	def update(self, currentTime):		
+		print('update: ', currentTime)
+		
+		# do clicks
+		if self.isRunning:
+			self.runScript()
+
+		if self.logCursor:
+			self.logCurrentPos()
+
+	def logCurrentPos(self):
+		cord = mouse.get_position()
+		self.callback(' cursor cord: ' +str(cord))
+
+	def runScript(self):
+		x,y =(0,0)
+		click(x,y)
+		
