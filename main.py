459da2c88504047d782d4570590c35ffa8343b00
intelligentchild <shiv.kurtartkar@gmail.com>
2020-10-17 16:52:17 +0530

    fix:

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 from tkinter import *
 from datetime import datetime
 
+import mouseController as mc
+
+maxListLen = 20
+
 def toggleState():
 	print('test')
 	print(actionBtnTxt.get())
 	isRunning = (actionBtnTxt.get()=='Start')
+	if isRunning:
+		mc.loop()
 	btnTxt =  'Stop' if isRunning else 'Start'
 	actionBtnTxt.set(btnTxt)
 	now = datetime.now()
 	log = str(now)+' '+btnTxt
 	logList.insert(END, log)
-
+	length = logList.size()
+	if length >= maxListLen:
+		logList.delete(0)
+	logList.see(END)
 
 root = Tk()
 root.title('Automate')
+
+root.columnconfigure(1,weight=1)
+root.rowconfigure(1,weight=1)
 
 # Parent widget for buttons
 btnFrame = Frame(root)
@@ -28,10 +40,13 @@ actionBtn.grid(row=0,column=0, padx=(10), pady=10)
 logFrame = LabelFrame(root, text='Logs', padx=5, pady=5)
 logFrame.grid(row=1, column=0, columnspan=3, padx=10,pady=10, sticky=E+W+N+S)
 
+logFrame.columnconfigure(0, weight=1)
+logFrame.rowconfigure(0,  weight=1)
+
 logScrollbar = Scrollbar(logFrame)
-logScrollbar.pack(side=RIGHT, fill=Y, expand=1)
 logList = Listbox(logFrame, yscrollcommand = logScrollbar.set)
-logList.pack(side=LEFT, fill=BOTH, expand=1)
+logList.grid(row=0,column=0, sticky=E+W+N+S)
+logScrollbar.grid(row=0, column=1, sticky=E+W+N+S)
 logScrollbar.config(command=logList.yview)
 
 root.mainloop()
---
+++ mouseController.py
@@ -0,0 +1,8 @@
+import mouse
+
+def click():
+	print("Click")
+
+def loop():
+	for i in range (10):
+		click()
